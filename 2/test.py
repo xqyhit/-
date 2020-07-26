ea4c5540278a61ed60a0efd8adfc85d7a683cfc4
xqyhit <hiteexqy@outlook.com>
2020-07-26 15:50:25 +0800

    第一次编写

---
+++ 2/test.py
@@ -0,0 +1,28 @@
+from maze_env import Maze
+from RL_brain import QLearningTable
+
+
+def update():
+    for episode in range(100):
+        # initial observation
+        observation = env.reset()
+
+        while True:
+            # fresh env
+            env.render()
+
+            # RL choose action based on observation
+            action = RL.choose_action(str(observation))
+
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            # RL learn from this transition
+            RL.learn(str(observation), action, reward, str(observation_))
+
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
